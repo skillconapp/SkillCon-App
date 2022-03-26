a7be4c7a708db500e4f9c6a767c979ce3445fac3
RohanBindu <72960029+RohanBindu@users.noreply.github.com>
2022-03-26 14:38:26 -0700

    Initial Commit

---
+++ SkillCon/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SkillCon
+//
+//  Created by Rohan Bindu on 3/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SkillCon/SkillConApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkillConApp.swift
+//  SkillCon
+//
+//  Created by Rohan Bindu on 3/26/22.
+//
+
+import SwiftUI
+
+@main
+struct SkillConApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

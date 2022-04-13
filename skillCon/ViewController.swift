814746a6a7c90fa81ca5fe02a9d40f6f8757d8cb
harshagarwal <harshagarwal2002@gmail.com>
2022-04-13 17:22:33 -0500

    Initial Commit

---
+++ skillCon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  skillCon
+//
+//  Created by Harsh Agarwal on 4/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
